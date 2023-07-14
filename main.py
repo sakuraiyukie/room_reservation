d81d6b3a28ccb9587dc89984927dd5e318f74ecc
sakuraiyukie <f2bm.4-b0001-ja0112kof_rotter@docomo.ne.jp>
2023-07-14 22:53:08 +0900

    FastAPIにPOSTメソッドを追加する

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
+
+class User(BaseModel):
+    user_id: int
+    user_name: str = Field(max_length=12)
+
 
 app = FastAPI()
 
 @app.get("/")
 async def index():
-    return{"message": "Success"}
+    return {"message": "Success"}
+
+@app.post("/users")
+async def users(users:User):
+    return {"code":200, "users": users}
+
