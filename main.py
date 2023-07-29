d1a3c9e1a9f931a1831f31b0a4e20f904b7d305b
sakuraiyukie <f2bm.4-b0001-ja0112kof_rotter@docomo.ne.jp>
2023-07-29 15:13:06 +0900

    画面を追加していく

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+import datetime
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 
+class Booking(BaseModel):
+    booking_id: int
+    user_id: int
+    room_id: int
+    booked_num: int
+    start_datetime: datetime.datetime
+    end_datetime: datetime.datetime
+
 class User(BaseModel):
     user_id: int
-    user_name: str = Field(max_length=12)
+    user_name: str = Field(max_length=24)
+
+class Room(BaseModel):
+    room_id: int
+    room_name: str = Field(max_length=24)
+    capacity: int
 
 
 app = FastAPI()
@@ -14,6 +28,13 @@ async def index():
 
 @app.post("/users")
 async def users(users:User):
+    return {"users": users}
 
-    return {"code":200, "users": users}
+@app.post("/rooms")
+async def rooms(rooms:Room):
+    return {"rooms": rooms}
+
+@app.post("/bookings")
+async def bookings(bookings:Booking):
+    return {"bookings": bookings}
 
