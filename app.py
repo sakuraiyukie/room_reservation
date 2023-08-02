cfad1e1af26dd516c977b7ddb607332eab504a3b
sakuraiyukie <f2bm.4-b0001-ja0112kof_rotter@docomo.ne.jp>
2023-08-03 01:09:35 +0900

    データベースを構築する

--- app.py
+++ app.py
@@ -8,21 +8,16 @@ import json
 page = st.sidebar.selectbox("Choose your page", ["users", "rooms", "bookings"])
 
 if page == "users":
-    st.title("APIテスト画面（ユーザー）")
+    st.title("ユーザー登録画面")
 
     with st.form(key="user"):
-        user_id: int = random.randint(0, 10)
         user_name: str = st.text_input("ユーザー名", max_chars=24)
         data = {
-            "user_id": user_id,
             "user_name": user_name,
         }
         submit_button = st.form_submit_button(label="リクエスト送信")
 
     if submit_button:
-        st.write("## 送信データ")
-        st.json(data)
-        st.write("## レスポンス結果")
         url = "http://127.0.0.1:8000/users"
         res = requests.post(
             url,
@@ -30,9 +25,9 @@ if page == "users":
             headers={"Content-Type": "application/json; charset=utf-8"},
             verify=False
         )
-        st.write(res.status_code)
+        if res.status_code == 200:
+            st.success("ユーザー登録完了")
         st.json(res.json())
-
 
 elif page == "rooms":
     st.title("APIテスト画面（会議室）")
@@ -99,7 +94,7 @@ elif page == "bookings":
     if submit_button:
         st.write("## 送信データ")
         st.json(data)
-        st.write("## レスポンス結果") 
+        st.write("## レスポンス結果")
         url = "http://127.0.0.1:8000/bookings"
         res = requests.post(
             url,
@@ -109,5 +104,4 @@ elif page == "bookings":
         )
         st.write(res.status_code)
         st.json(res.json())
-
 
